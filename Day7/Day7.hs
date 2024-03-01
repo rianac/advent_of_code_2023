f874887e364108bd54a8f5d8e2c91d9d65b90403
Marian Mach <marian.mach@tuke.sk>
2024-03-01 23:09:14 +0100

    Polishing code

--- Day7/Day7.hs
+++ Day7/Day7.hs
@@ -9,17 +9,18 @@ data Card
   | Card9 | CardT | CardJ | CardQ | CardK | CardA
   deriving (Show, Eq, Ord)
 
-data Hand
-  = High [Card] | OnePair [Card] | TwoPairs [Card] | ThreeOfKind [Card]
-  | FullHouse [Card] | FourOfKind [Card] | FiveOfKind [Card]
+data HandType
+  = High | OnePair | TwoPairs | ThreeOfKind | FullHouse | FourOfKind | FiveOfKind
   deriving (Show, Eq, Ord)
 
+data Hand = Hand HandType [Card] deriving (Show, Eq, Ord)
+
 instance Read Hand where
-  readsPrec _ [a,b,c,d,e] =
-    let cards = map findCard [a,b,c,d,e]
-        cardsNoJoker = filter (/=CardJoker) cards
-        hand_type = findHand . sort . map length . group . sort $ cardsNoJoker
-    in [(hand_type cards,"")]
+  readsPrec _ [a, b, c, d, e] =
+    let cards        = map findCard [a, b, c, d, e]
+        cardsNoJoker = filter (/= CardJoker) cards
+        hand_type    = findHand . sort . map length . group . sort $ cardsNoJoker
+    in [(Hand hand_type cards, "")]
     where
       findCard x = fromMaybe (error "unknown card") $ lookup x card_map
       findHand x = case filter (elem x . fst) hand_map of
@@ -28,16 +29,16 @@ instance Read Hand where
       card_map = [('A',CardA),('K',CardK),('Q',CardQ),('J',CardJ),('T',CardT),
                   ('9',Card9),('8',Card8),('7',Card7),('6',Card6),('5',Card5),
                   ('4',Card4),('3',Card3),('2',Card2),('*',CardJoker)]
-      hand_map = [([[5],[4],[3],[2],[1],[]],FiveOfKind),
-                  ([[1,4],[1,3],[1,2],[1,1]],FourOfKind),
-                  ([[2,3],[2,2]],FullHouse),
-                  ([[1,1,3],[1,1,2],[1,1,1]],ThreeOfKind),
-                  ([[1,2,2]],TwoPairs),
-                  ([[1,1,1,2],[1,1,1,1]],OnePair),
-                  ([[1,1,1,1,1]],High)]
+      hand_map = [([[5], [4], [3], [2], [1], []],     FiveOfKind),
+                  ([[1, 4], [1, 3], [1, 2], [1, 1]],  FourOfKind),
+                  ([[2, 3], [2, 2]],                  FullHouse),
+                  ([[1, 1, 3], [1, 1, 2], [1, 1, 1]], ThreeOfKind),
+                  ([[1, 2, 2]],                       TwoPairs),
+                  ([[1, 1, 1, 2], [1, 1, 1, 1]],      OnePair),
+                  ([[1, 1, 1, 1, 1]],                 High)]
 
-parseData :: String -> [(Hand,Int)]
-parseData = map ((\[a,b] -> (read a,read b)) . words) . lines
+parseData :: String -> [(Hand, Int)]
+parseData = map ((\[a, b] -> (read a, read b)) . words) . lines
 
 {- Task 1:
 To order five-card hands - ordering is primarily based on type of hands
@@ -45,12 +46,10 @@ and secondarily on type of cards included in hands (in case it is not
 possible to decide using the first ordering).
 -}
 
-calculateWinning :: [(Hand,Int)] -> Int
-calculateWinning =
-  sum . map (\(r,(_,b)) -> r * b) . zip [1..] . sort
+calculateWinning :: [(Hand, Int)] -> Int
+calculateWinning = sum . map (\(r, (_, b)) -> r * b) . zip [1..] . sort
 
-task1 filename =
-  calculateWinning . parseData <$> readFile filename
+task1 filename = calculateWinning . parseData <$> readFile filename
 
 testTask1 :: IO ()
 testTask1 = do
@@ -67,8 +66,7 @@ be replaced by any other card.
 replaceJ :: String -> String
 replaceJ = intercalate "*" . splitOn "J"
 
-task2 filename =
-  calculateWinning . parseData . replaceJ <$> readFile filename
+task2 filename = calculateWinning . parseData . replaceJ <$> readFile filename
 
 testTask2 :: IO ()
 testTask2 = do
