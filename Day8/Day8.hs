306a85613f7550240b442c48fa59e42f2e632f21
Marian Mach <marian.mach@tuke.sk>
2024-03-02 12:01:16 +0100

    Code for day 8

---
+++ Day8/Day8.hs
@@ -0,0 +1,75 @@
+module Main where
+
+import Data.Maybe (fromJust)
+
+type Node = (String, (String, String))
+
+-- Parse input file
+
+removeChars :: String -> String -> String
+removeChars forbiden = filter (not . (flip elem forbiden))
+
+parseData :: String -> (String, [Node])
+parseData input =
+  let (plan, _ : _ : nodes) = span (/= '\n') input
+      makeNode [a, b, c] = (a, (b, c))
+  in (plan, map (makeNode . parseNodes) $ lines nodes)
+  where parseNodes = words . removeChars "=(,)"
+
+{- Task 1
+To find the shortest path from node AAA to node ZZZ.
+-}
+
+-- Find all possible continuations from a given node
+nextState :: String -> [Node] -> (String,String)
+nextState state instructions = fromJust $ lookup state instructions
+
+-- Length calculation for graph traversal following given step sequence
+runStates :: (String -> Bool) -> String -> Int -> [Node] -> String -> Int
+runStates f (s:steps) c instr x
+  | f x = c
+  | s == 'L' = runStates f steps (c + 1) instr (fst $ nextState x instr)
+  | s == 'R' = runStates f steps (c + 1) instr (snd $ nextState x instr)
+  | otherwise = error "Unknown direction"
+
+-- Prepare conditions for graph traversal
+followPlan :: (String, [Node]) -> Int
+followPlan (plan, nodes) = runStates (=="ZZZ") (cycle plan) 0 nodes "AAA" 
+
+task1 filename = followPlan . parseData <$> readFile filename
+
+testTask1 :: IO ()
+testTask1 = do
+  res1 <- task1 "day8-example1"
+  res2 <- task1 "day8-example2"
+  case (res1,res2) of
+    (2,6) -> putStrLn "OK"
+    _     -> putStrLn "something went wrong"
+
+{- Task 2
+To find all shortest paths from nodes **A to nodes **Z such, that all have the
+same length.
+-}
+
+-- Prepare conditions for several traversals and calculate number of
+-- steps for their coordinated finishing at the same time
+followGhoust (plan, nodes) =
+  let xs = map fst $ filter (\x -> (last . fst) x == 'A') nodes
+  in foldl1 lcm $ map (runStates (\x -> last x == 'Z') (cycle plan) 0 nodes) xs
+
+task2 filename = followGhoust . parseData <$> readFile filename
+
+testTask2 :: IO ()
+testTask2 = do
+  res <- task2 "day8-example3"
+  case res of
+    6 -> putStrLn "OK"
+    _ -> putStrLn "something went wrong"
+
+main :: IO ()
+main = do
+  x <- task1 "day8-input"
+  y <- task2 "day8-input"
+  putStrLn $ "Task 1: " ++ show x
+  putStrLn $ "Task 2: " ++ show y
+
---
+++ Day8/README.md
@@ -0,0 +1,10 @@
+## Day 8
+
+Given a graph of nodes where each node is connected with two other nodes, the task reduces to finding the length of a path in the graph while following a given sequence of steps. Easy parsing into simple data structures (I used only tuples and lists) using a few lines of code (no need to use any parser combinators library).
+
+The first task (one path) was easy - simple graph traversal. The second task (several coordinated paths) made no difficulties while extending the code used for the first task. Unfortunately, the code was not usable for processing the given input data - it just swallowed all memory of my computer and continued at snail's pace. Reddit helped me in better task understanding - each path was cyclic (can be repeatedly iterated) and considering the given step sequence, the paths were independent. As a result, I found all paths independently and just find LSM (least common multiple) of their lengths.
+
+## What I have learnt:
+
+- Haskell's sensitivity for algorithm selection
+
