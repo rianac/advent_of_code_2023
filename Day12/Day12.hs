4f3e906f00cd0fa147db7015a8b6e50d801b2751
Marian Mach <marian.mach@tuke.sk>
2024-03-10 11:01:29 +0100

    Code for day 12

---
+++ Day12/Day12.hs
@@ -0,0 +1,96 @@
+module Main where
+
+import Data.List (intersperse)
+import Data.List.Split (splitOn)
+import Text.ParserCombinators.ReadP
+
+-- Parsing input file --
+
+parseData :: String -> [(String,[Int])]
+parseData xs =
+  zip (map (!! 0) records) (map (map read .  splitOn "," . (!! 1)) records)
+  where
+    records =  map  words . lines $ xs
+
+-- Task 1 --
+
+damagedP :: Int -> ReadP [Char]
+damagedP x = count x $ satisfy (`elem` "?#")
+
+ok1P, okP :: ReadP [Char]
+ok1P       = many1 $ satisfy (`elem` ".?")
+okP        = many $ satisfy (`elem` ".?")
+
+-- Prepare a list of required parsers
+makeParsersP :: [Int] -> [ReadP [Char]]
+makeParsersP xs = intersperse ok1P (map damagedP xs)
+
+-- Combine parsers into a sequence
+composeParsersP :: [ReadP [Char]] -> ReadP [[Char]]
+composeParsersP [a,b,c] =
+  let fun _ _ _ = []
+  in fun <$> a <*> b <*> c
+composeParsersP [a,b,c,d,e] =
+  let fun _ _ _ _ _ = []
+  in fun <$> a <*> b <*> c <*> d <*> e
+composeParsersP [a,b,c,d,e,f,g] =
+  let fun _ _ _ _ _ _ _ = []
+  in fun <$> a <*> b <*> c <*> d <*> e <*> f <*> g
+composeParsersP [a,b,c,d,e,f,g,h,i] =
+  let fun _ _ _ _ _ _ _ _ _ = []
+  in fun <$> a <*> b <*> c <*> d <*> e <*> f <*> g <*> h <*> i
+composeParsersP [a,b,c,d,e,f,g,h,i,j,k] =
+  let fun _ _ _ _ _ _ _ _ _ _ _ = []
+  in fun <$> a <*> b <*> c <*> d <*> e <*> f <*> g <*> h <*> i <*> j <*> k
+composeParsersP _ = error "Unpredicted number of contiguous groups"
+
+-- Count the number of possible arrangements for all patterns
+countAllArrangements :: [(String, [Int])] -> Int
+countAllArrangements = sum . map (length . match)
+  where
+    match (x, y) = readP_to_S (finalizeParser $ makeParser y) x
+    makeParser = composeParsersP . makeParsersP
+    finalizeParser p = okP *> p <* okP <* eof
+
+task1 filename =  countAllArrangements . parseData <$> readFile filename
+
+-- Task 2 --
+
+-- Count the number of possible arrangements for one pattern
+countArrangements :: (String, [Int]) -> Int
+countArrangements (x, y) =
+  length $ readP_to_S (prepareParser y) x
+  where
+    makeParser = foldl1 (*>) . intersperse ok1P . map damagedP
+    finalizeParser p = okP *> p <* okP <* eof
+    prepareParser = finalizeParser . makeParser
+
+-- Unfold data and count the number of possible arrangements for all patterns
+countAllArrangements2 :: Int -> [([Char], [Int])] -> Int
+countAllArrangements2 r = sum . map (countArrangements . makeUnfolded)
+  where
+    makeUnfolded (x, y) = (unfoldData x, unfoldGroups y)
+    unfoldGroups = concat . replicate r
+    unfoldData = concat . intersperse "?" . replicate r
+
+task1alt filename =  countAllArrangements2 1 . parseData <$> readFile filename
+
+task2 filename =  countAllArrangements2 5 . parseData <$> readFile filename
+
+-- Useful staff --
+
+testTask1 =
+  (\x -> if x == 21 then "OK" else "Something went wrong")
+    <$> task1 "day12-example"
+
+testTask2 =
+  (\x -> if x == 525152 then "OK" else "Something went wrong")
+    <$> task2 "day12-example"
+
+main :: IO ()
+main = do
+  x <- task1 "day12-input"
+  x2 <- task1 "day12-input"
+  y <- task2 "day12-input"
+  putStrLn $ "Task 1: " ++ show x ++ " or " ++ show x2
+  putStrLn $ "Task 2: " ++ show y
---
+++ Day12/README.md
@@ -0,0 +1,12 @@
+## Day 12
+
+The problem reminded me a parsing problem. Therefore, I decided to solve it using Haskell's tools for parsing. First, I tried to use regexes but with no success. I was not able to produce overlapping matches (e.g. both matches "?##" and "##?" for a group of 3 over the pattern "?##?"). Thus, I abandoned this approach.
+
+Next, I tried to use parser combinators. After longer tinkering a bit with how to combine parsers into sequences, I came with two solutions - one based on `<*>` (suitable if parsed values are relevant) and the other based on `*>` (if only number of matches is relevant but not parsed values). Both approaches worked well for the first task. Regarding the second task - there was no problem with the given example. But I hit the scalability wall when processing the given input. My program consumed all computer memory with subsequent swapping.
+
+To solve the second task, the approach based on parser combinators is not viable.
+
+## What I have learnt:
+
+- use parsing combinators for a task of getting one arrangement and not many (especially not millions alternative arrangements)
+- new functions (`!!')
